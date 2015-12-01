ddca6ed897c9d6dd39ad3412690bcba676145d06
Stanghelle <post@stanghelle.net>
2015-12-01 15:31:49 +0100

    add new js

---
+++ www/js/timerjson.js
@@ -0,0 +1,48 @@
+function ajaxFunction(){
+
+   var ajaxRequest;  // The variable that makes Ajax possible!
+   try{
+   
+      // Opera 8.0+, Firefox, Safari
+      ajaxRequest = new XMLHttpRequest();
+   }catch (e){
+      
+      // Internet Explorer Browsers
+      try{
+         ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+      }catch (e) {
+         
+         try{
+            ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+         }catch (e){
+         
+            // Something went wrong
+            alert("Your browser broke!");
+            location.refresh(true);
+            return false;
+         }
+      }
+   }
+   
+   // Create a function that will receive data
+   // sent from the server and will update
+   // div section in the same page.
+   ajaxRequest.onreadystatechange = function(){
+   
+      if(ajaxRequest.readyState == 4){
+         var ajaxDisplay = document.getElementById('ajaxDiv');
+         ajaxDisplay.innerHTML = ajaxRequest.responseText;
+      }
+   }
+   
+   // Now get the value from user and pass it to
+   // server script.
+   var user_id = document.getElementById('user_id').value;
+   var mnd = document.getElementById('mnd').value;
+   var queryString = "?user_id=" + user_id + "&mnd=" + mnd;
+   
+   queryString +=  "&mnd=" + mnd ;
+   ajaxRequest.open("GET", "http://api.simplehours.no/json.php" + queryString, true );
+   ajaxRequest.send(null); 
+   
+}
